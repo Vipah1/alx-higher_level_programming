419336e584beed00485214dc25806e32e70818d3
Osman <oselibas@gmail.com>
2023-08-10 15:37:38 +0000

    Calculation

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import calculator_1 as calc
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    add = calc.add(a, b)
+    sub = calc.sub(a, b)
+    mul = calc.sub(a, b)
+    div = calc.sub(a, b)
+    print("{a} + {b} = {add}")
+    print("{a} - {b} = {sub}")
+    print("{a} * {b} = {mul}")
+    print("{a} / {b} = {div}")
