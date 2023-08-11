7382016a7b26eb25b4b9e3eb65ece0344f03bde4
Osman <oselibas@gmail.com>
2023-08-11 16:06:03 +0000

    Infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    count = len(agrv)
+    for i in range(0, count):
+        sum = i + int(argv[i])
+        i+= 1
+    print("{}".format(sum))
