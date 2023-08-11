53e90711fba1edc39eacecd161472a66480a4f1e
Osman <oselibas@gmail.com>
2023-08-11 15:54:43 +0000

    Argv fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 from sys import argv
 if __name__ == "__main__":
     count = len(argv)
-    if count > 1:
-        print("{} arguments:".format(count))
-    elif count == 1:
-        print("{} argument:".format(count))
+    if count == 1:
+        print("{} arguments.".format(count - 1))
+    elif count == 2:
+        print("{} argument:".format(count - 1))
     else:
-        print("{} arguments.".format(count))
+        print("{} arguments.".format(count - 1))
     for i in range(1, count):
         print("{}: {}".format(i, argv[i]))
