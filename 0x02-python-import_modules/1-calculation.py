02631a8117ad039ca4874691dc3db6eaf0d3f75a
Osman <oselibas@gmail.com>
2023-08-11 17:03:54 +0000

    Fixing calculator agaiin

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import calculator_1 as calc
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
-    addi = calc.add(a, b)
-    subi = calc.sub(a, b)
-    muli = calc.mul(a, b)
-    divi = calc.div(a, b)
+    addi = add(a, b)
+    subi = sub(a, b)
+    muli = mul(a, b)
+    divi = div(a, b)
     print("{} + {} = {}".format(a, b, addi))
     print("{} - {} = {}".format(a, b, subi))
     print("{} * {} = {}".format(a, b, muli))
