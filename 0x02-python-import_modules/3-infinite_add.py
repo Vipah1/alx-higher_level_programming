4148b04bd0d916e464098a64b3672a584df1e21a
Osman <oselibas@gmail.com>
2023-08-11 16:32:59 +0000

    This is it

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,5 @@ from sys import argv
 if __name__ == "__main__":
     count = len(argv)
     for i in range(0, count):
-        c_sum = i + int(argv[i])
-        i += 1
+        c_sum += int(argv[i])
     print("{}".format(c_sum))
