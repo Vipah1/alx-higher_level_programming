49c9b2a66634ffe20bbb9e38ff9f514452909e05
Osman <oselibas@gmail.com>
2023-08-13 20:06:01 +0000

    New in list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+def new_in_list(my_list, idx, element):
+    count = len(my_list)
+    rep = my_list[:]
+    if idx < 0:
+        return my_list
+    elif idx > count - 1:
+        return my_list
+    else:
+        rep[idx] = element
+        return rep[:]
