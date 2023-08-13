f6af96e9b90a8c9550856e4a0cb492a9c9c3dbdb
Osman <oselibas@gmail.com>
2023-08-13 20:47:33 +0000

    No C fixed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    count = len(my_sring)
-    not_c = 'cC'
-    new_str = list(my_string)
-    for i in range(count):
-        if new_str[i] in not_c:
-            new_str[i] = ""
-    return "".join(new_string)
+    new_str = ''
+    for i in my_string:
+        if (i != 'c') and (i != 'C'):
+            new_str += i
+    return new_str
