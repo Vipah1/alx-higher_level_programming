736a96758e24ff28cd0605897aa256319f68ca24
Osman <oselibas@gmail.com>
2023-08-16 17:25:27 +0000

    Onlly different elements fixed.

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    o_diff = set_1 - set_2
+    o_diff = set()
+    
+    for i in set_1:
+        if i not in set_2:
+            o_diff.add(i)
+
+    for i in set_2 and not in o_diff:
+        if i not in set_1:
+            o_diff.add(i)
     
     return o_diff
