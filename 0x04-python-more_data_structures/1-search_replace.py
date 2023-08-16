b6e544b754858ab7dbb6e59bee69050d14fe4687
Osman <oselibas@gmail.com>
2023-08-16 18:11:49 +0000

    Fixing serach and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     newL = []
-    for i in my_list:
-        if i == search:
-            i = replace
-            newL.append(i)
-    return newL
+    if my_list:
+        for i in my_list:
+            if i == search:
+                i = replace
+                newL.append(i)
+            else:
+                newL.append(i)
+        return newL
+    else:
+        return my_list
