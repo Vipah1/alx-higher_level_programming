4d256078a3a4c8a7d353715b7417ab04b7bed141
Osman <oselibas@gmail.com>
2023-08-28 14:31:18 +0000

    Error handling

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         while count < x:
             print("{:d}".format(my_list[count]), end="")
             count += 1
-    except Exception:
-        pass
+    except (TypeError, ValueError):
+        continue
     print()
     return count
