0d6bc689c06dcb60af5a6d8491969a8e72ffe22e
Osman <oselibas@gmail.com>
2023-12-01 13:56:04 -0800

    Post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+POST request to the passed Url with the email as parameter
+"""
+import urllib.request
+from sys import argv
+
+def main(argv):
+    """
+    """
+    values = {'email': argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf8')
+    url = argv[1]
+    req = urllibe.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        result = request.read()
+        print(result.decode('utf8'))
+
+if __name__ as "__main__":
+    main(argv)
